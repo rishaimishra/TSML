995ba2200fbb3f81163930d11103d6ad258f7888
Partha <partha.beas@gmail.com>
2022-09-02 18:08:03 +0530

    partha

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'assign.guard' => \App\Http\Middleware\AssignGuard::class,
+        'jwtmiddleware' => \App\Http\Middleware\JWTMiddleware::class,
 
     ];
 
