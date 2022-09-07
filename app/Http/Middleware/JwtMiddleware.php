5898049426a3f29cb11d227ab5ff4a6c54eae9ed
Partha <partha.beas@gmail.com>
2022-09-07 12:25:26 +0530

    API Changes

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -15,7 +15,7 @@ class JWTMiddleware extends BaseMiddleware
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
-    */
+     */
     public function handle($request, Closure $next)
     {
         try {
