0cdbe2dce6f7bfb15a2df7f1a0ee3343a7cf9900
Partha <partha.beas@gmail.com>
2022-09-07 16:54:30 +0530

    API Changes

--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,20 @@ Route::post('password-update', 'Auth\ResetPasswordController@reset')->name('user
 Route::group(['namespace'=>'Api\Modules'],function(){
 
 	// Category Routes....
-	Route::post('store-category', 'Category\CategoryController@storeCategory')->name('store_category');
-	Route::get('category-list', 'Category\CategoryController@categoryList')->name('category_list');
-	Route::put('edit-category/{catId}', 'Category\CategoryController@editCategory')->name('edit_category');
-	Route::get('inactive-category/{catId}', 'Category\CategoryController@inactiveCategory')->name('inactive_category');
-	Route::get('active-category/{catId}', 'Category\CategoryController@activeCategory')->name('active_category');
-	Route::get('category-list-my', 'Category\CategoryController@categoryListMy')->name('category_list_my');
+	// Route::post('store-category', 'Category\CategoryController@storeCategory')->name('store_category');
+	// Route::get('category-list', 'Category\CategoryController@categoryList')->name('category_list');
+	// Route::put('edit-category/{catId}', 'Category\CategoryController@editCategory')->name('edit_category');
+	// Route::get('inactive-category/{catId}', 'Category\CategoryController@inactiveCategory')->name('inactive_category');
+	// Route::get('active-category/{catId}', 'Category\CategoryController@activeCategory')->name('active_category');
+	// Route::get('category-list-my', 'Category\CategoryController@categoryListMy')->name('category_list_my');
 
-	// Sub Category Routes ....
-	Route::post('store-sub-category', 'SubCategory\SubCategoryController@storeSubCategory')->name('store_sub_category');
-	Route::get('sub-category-list', 'SubCategory\SubCategoryController@subCategoryList')->name('sub_category_list');
-	Route::put('edit-sub-category/{subCatId}', 'SubCategory\SubCategoryController@editSubCategory')->name('edit_sub_category');
-	Route::get('inactive-sub-category/{subCatId}', 'SubCategory\SubCategoryController@inactiveSubCategory')->name('inactive_sub_category');
-	Route::get('active-sub-category/{subCatId}', 'SubCategory\SubCategoryController@activeSubCategory')->name('active_sub_category');	
-	Route::get('sub-category-list-my', 'SubCategory\SubCategoryController@subCategoryListMy')->name('sub_category_list_my');
+	// // Sub Category Routes ....
+	// Route::post('store-sub-category', 'SubCategory\SubCategoryController@storeSubCategory')->name('store_sub_category');
+	// Route::get('sub-category-list', 'SubCategory\SubCategoryController@subCategoryList')->name('sub_category_list');
+	// Route::put('edit-sub-category/{subCatId}', 'SubCategory\SubCategoryController@editSubCategory')->name('edit_sub_category');
+	// Route::get('inactive-sub-category/{subCatId}', 'SubCategory\SubCategoryController@inactiveSubCategory')->name('inactive_sub_category');
+	// Route::get('active-sub-category/{subCatId}', 'SubCategory\SubCategoryController@activeSubCategory')->name('active_sub_category');	
+	// Route::get('sub-category-list-my', 'SubCategory\SubCategoryController@subCategoryListMy')->name('sub_category_list_my');
 
 	// Product Routes ....
 	Route::post('store-product', 'Product\ProductController@storeProduct')->name('store_product');
@@ -76,8 +76,38 @@ Route::group(['namespace'=>'Api\Modules'],function(){
 // Admin Routes....
 Route::post('admin-login', 'AdminAuthController@Adminlogin')->name('admin_login');
 Route::post('admin-register', 'AdminAuthController@Adminregister')->name('admin_register');
-Route::group(['prefix' => 'admin','middleware' => ['assign.guard:admins','jwtmiddleware']],function ()
-{
+
+Route::group(['prefix' => 'admin','middleware' => ['assign.guard:admins','jwtmiddleware']],function (){
 	Route::get('/demo','AdminController@demo');	
 	Route::post('admin-logout', 'AdminController@logout')->name('admin_logout');
+
+	Route::group(['namespace'=>'Api\Modules'],function(){
+
+		// Category Routes....
+		Route::post('store-category', 'Category\CategoryController@storeCategory')->name('store_category');
+		Route::get('category-list', 'Category\CategoryController@categoryList')->name('category_list');
+		Route::put('edit-category/{catId}', 'Category\CategoryController@editCategory')->name('edit_category');
+		Route::get('inactive-category/{catId}', 'Category\CategoryController@inactiveCategory')->name('inactive_category');
+		Route::get('active-category/{catId}', 'Category\CategoryController@activeCategory')->name('active_category');
+		Route::get('category-list-my', 'Category\CategoryController@categoryListMy')->name('category_list_my');
+
+		// Sub Category Routes ....
+		Route::post('store-sub-category', 'SubCategory\SubCategoryController@storeSubCategory')->name('store_sub_category');
+		Route::get('sub-category-list', 'SubCategory\SubCategoryController@subCategoryList')->name('sub_category_list');
+		Route::put('edit-sub-category/{subCatId}', 'SubCategory\SubCategoryController@editSubCategory')->name('edit_sub_category');
+		Route::get('inactive-sub-category/{subCatId}', 'SubCategory\SubCategoryController@inactiveSubCategory')->name('inactive_sub_category');
+		Route::get('active-sub-category/{subCatId}', 'SubCategory\SubCategoryController@activeSubCategory')->name('active_sub_category');	
+		Route::get('sub-category-list-my', 'SubCategory\SubCategoryController@subCategoryListMy')->name('sub_category_list_my');
+
+		// Product Routes ....
+		Route::post('store-product', 'Product\ProductController@storeProduct')->name('store_product');
+		Route::put('edit-product/{proId}', 'Product\ProductController@editProduct')->name('edit_product');
+		Route::get('delete-product/{proId}', 'Product\ProductController@deleteProduct')->name('delete_product');
+		Route::get('product-list', 'Product\ProductController@productList')->name('product_list');
+		Route::get('inactive-product/{proId}', 'Product\ProductController@inactiveProduct')->name('inactive_product');
+		Route::get('active-product/{proId}', 'Product\ProductController@activeProduct')->name('active_product');
+		Route::get('product-list-my', 'Product\ProductController@productListMy')->name('product_list_my');
+
+	});
+
 });
