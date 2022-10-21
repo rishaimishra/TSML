1a6921e2562fbc4fe5b891ad8fe30bc116374639
Partha <partha.beas@gmail.com>
2022-10-21 16:26:05 +0530

    price calculate

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,8 @@ Route::group(['namespace'=>'Api\Modules'],function(){
    		 Route::get('delete_quote_by_id/{id}','Quote\QuoteController@deleteQuoteById');
    		 Route::get('get_quote_sche_by_id/{id}','Quote\QuoteController@getQuoteScheById');
    		 Route::post('delete_quote_sche','Quote\QuoteController@deleteQuoteSche');
+
+   		 Route::post('get-store-pro-price','PriceManagement\PriceManagementController@getProPrice')->name('get_store_pro_price');
      });
    	
 });
