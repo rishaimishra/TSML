2b204d53b720c9876db2afcb1ace0f9914c93311
Sourav 88 <srvmondal88@gmail.com>
2022-10-28 16:50:12 +0530

    quote status updates

--- app/Http/Controllers/Api/Modules/Quote/QuoteController.php
+++ app/Http/Controllers/Api/Modules/Quote/QuoteController.php
@@ -537,7 +537,8 @@ class QuoteController extends Controller
            $status = $value['status'];
                  // echo "<pre>";print_r($id);exit();
 
-           $updated = QuoteSchedule::where('id',$id)->update(['quote_status' => $status]);
+           // $updated = QuoteSchedule::where('id',$id)->update(['quote_status' => $status]);
+           $updated = DB::table('quote_schedules')->where('schedule_no',$id)->update(['quote_status' => $status]);
          }
          
          
