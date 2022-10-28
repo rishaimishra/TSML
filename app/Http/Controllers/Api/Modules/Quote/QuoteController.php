c50cabadc4fac1e575ddfb52f08e966849de1c65
Sourav 88 <srvmondal88@gmail.com>
2022-10-28 16:14:35 +0530

    kam rfq list

--- app/Http/Controllers/Api/Modules/Quote/QuoteController.php
+++ app/Http/Controllers/Api/Modules/Quote/QuoteController.php
@@ -930,7 +930,10 @@ class QuoteController extends Controller
 
          $quotes = DB::table('quotes')->leftjoin('users','quotes.user_id','users.id')
          ->leftjoin('quote_schedules','quotes.id','quote_schedules.quote_id')
-         ->select('quotes.*','users.name',DB::raw("(sum(quotes.quantity)) as tot_qt"),)
+         ->leftjoin('products','quotes.product_id','products.id')
+         ->leftjoin('categorys','quotes.cat_id','categorys.id')
+         ->leftjoin('sub_categorys','categorys.id','sub_categorys.cat_id')
+         ->select('quotes.*','users.name',DB::raw("(sum(quotes.quantity)) as tot_qt"),'products.pro_desc','quotes.rfq_no','categorys.cat_name','sub_categorys.sub_cat_name','categorys.primary_image')
          ->groupBy('quotes.rfq_no')
          ->orderBy('quotes.created_at','desc')
          ->where('quote_schedules.location',$user_state)
@@ -949,9 +952,12 @@ class QuoteController extends Controller
           $quoteArr[$key]['quantity'] = $value->tot_qt;
           $quoteArr[$key]['kam_status'] = $value->kam_status;
           $quoteArr[$key]['name'] = $value->name;
-          $quoteArr[$key]['created_at'] = date('d-m-Y',strtotime($value->created_at));
+          $quoteArr[$key]['created_at'] = date('jS F, Y',strtotime($value->created_at));
           $quoteArr[$key]['cat_id'] = $value->cat_id;
           $quoteArr[$key]['product_id'] = $value->product_id;
+          $quoteArr[$key]['cat_name'] = $value->cat_name;
+          $quoteArr[$key]['sub_cat_name'] = $value->sub_cat_name;
+          $quoteArr[$key]['pro_desc'] = $value->pro_desc;
           $quoteArr[$key]['status'] = $this->schedule_status($value->rfq_no);
                     // $quoteArr[$key]['schedules'] = $value->schedules;
                     // $quoteArr[$key]['product'] = $value->product;
