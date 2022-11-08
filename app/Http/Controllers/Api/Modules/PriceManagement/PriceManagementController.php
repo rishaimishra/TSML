6344940f2078bac164cf0f45fe8f0fc5186a2ef3
Partha <partha.beas@gmail.com>
2022-11-08 10:37:48 +0530

    multi changes

--- app/Http/Controllers/Api/Modules/PriceManagement/PriceManagementController.php
+++ app/Http/Controllers/Api/Modules/PriceManagement/PriceManagementController.php
@@ -460,25 +460,31 @@ class PriceManagementController extends Controller
     */
     public function getProPrice(Request $request)
     {
-        
-
         try{ 
             // $priceData = PriceCalculation::where('user_id',$request->user_id)->where('pro_id',$request->pro_id)->where('cat_id',$request->cat_id)->where('sub_cat_id',$request->sub_cat_id)->where('size',$request->size)->first();
 
             $priceData = PriceCalculation::where('user_id',$request->user_id)->where('pro_id',$request->pro_id)->where('cat_id',$request->cat_id)->where('size',$request->size)->first();
             // dd($priceData);
-            $getdeliverycost = Freights::where('pickup_from',$request->pickup_from)->where('location',$request->location)->where('destation_location',$request->destation_location)->first(); 
+            if (!empty($priceData)) {
               
-            $data['bpt_price'] = $priceData->BPT_Price;
-            $data['price_premium'] = $priceData->Price_Premium;
-            $data['misc_expense'] = $priceData->Misc_Expense;
-            $data['delivery_cost'] = $getdeliverycost->freight_charges; 
-            $data['interest_rate'] = $priceData->Interest_Rate;
-            $data['cam_discount'] = $priceData->CAM_Discount;
-           
-            if (!empty($getdeliverycost)) {
-               return response()->json(['status'=>1,'message' =>'success.','result' => $data],200);
-            }
+              $getdeliverycost = Freights::where('pickup_from',$request->pickup_from)->where('location',$request->location)->where('destation_location',$request->destation_location)->first(); 
+              
+              $data['bpt_price'] = $priceData->BPT_Price;
+              $data['price_premium'] = $priceData->Price_Premium;
+              $data['misc_expense'] = $priceData->Misc_Expense;
+              $data['delivery_cost'] = $getdeliverycost->freight_charges; 
+              $data['interest_rate'] = $priceData->Interest_Rate;
+              $data['cam_discount'] = $priceData->CAM_Discount;
+             
+              if (!empty($getdeliverycost)) {
+                 return response()->json(['status'=>1,'message' =>'success.','result' => $data],200);
+              }
+              else{
+
+               return response()->json(['status'=>1,'message' =>'No data found','result' => []],config('global.success_status'));
+
+              }
+            }            
             else{
 
                return response()->json(['status'=>1,'message' =>'No data found','result' => []],config('global.success_status'));
