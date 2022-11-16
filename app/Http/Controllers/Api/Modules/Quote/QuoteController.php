91626e65b12e0f6ba28aa4090015b5ee7279b86f
Sourav 88 <srvmondal88@gmail.com>
2022-11-16 12:11:30 +0530

    quotes updates

--- app/Http/Controllers/Api/Modules/Quote/QuoteController.php
+++ app/Http/Controllers/Api/Modules/Quote/QuoteController.php
@@ -239,7 +239,7 @@ class QuoteController extends Controller
           $sche['valid_till'] = $value['valid_till'];
           $sche['schedule_no'] = $value['schedule_no'];
           $sche['kamsRemarks'] = $value['kamsRemarks'];
-          $sche['confirm_date'] = $value['confirm_date'];
+          $sche['confirm_date'] = date('Y-m-d',strtotime($value['confirm_date']));
           
           // echo "<pre>";print_r($sche);exit();
 
