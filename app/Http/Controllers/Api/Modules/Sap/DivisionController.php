36f84ac0b26dd94c8374a72bc3575c99bcaf0540
Partha <partha.beas@gmail.com>
2022-12-09 16:38:10 +0530

    comfile

---
+++ app/Http/Controllers/Api/Modules/Sap/DivisionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Modules\Sap;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DivisionController extends Controller
+{
+    //
+}
