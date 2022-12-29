8e5710e166ea4854ed3d73044723cc45b2880e5a
Partha <partha.beas@gmail.com>
2022-12-29 18:17:04 +0530

    mail

--- routes/api.php
+++ routes/api.php
@@ -329,7 +329,7 @@ Route::group(['prefix' => 'admin','middleware' => ['assign.guard:admins','jwtmid
 
 		// Product Routes ....
 		Route::post('store-product', 'Product\ProductController@storeProduct')->name('store_product');
-		Route::put('edit-product/{proId}', 'Product\ProductController@editProduct')->name('edit_product');
+		Route::get('edit-product/{proId}', 'Product\ProductController@editProduct')->name('edit_product');
       Route::post('update-product/{proId}', 'Product\ProductController@updateProduct')->name('update_product');
 		Route::get('delete-product/{proId}', 'Product\ProductController@deleteProduct')->name('delete_product');
 		Route::get('product-list', 'Product\ProductController@productList')->name('product_list');
