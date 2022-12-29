920dff75a909920257f171e2891cf4a9de53d9db
Sourav 88 <srvmondal88@gmail.com>
2022-12-29 18:12:19 +0530

    plant notifications

--- app/Models/PlantNotification.php
+++ app/Models/PlantNotification.php
@@ -8,4 +8,11 @@ class PlantNotification extends Model
 {
     protected $table = 'plant_notifications';
     protected $guarded = [];
+
+    public function userName()
+    {
+        return $this->hasOne('App\User','id','sender_id');
+    }
 }
+
+
