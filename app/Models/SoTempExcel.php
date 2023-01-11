e612964b2dea7b20d2586ef8f337bb48fd06297d
Partha <partha.beas@gmail.com>
2023-01-11 12:13:06 +0530

    so excel

---
+++ app/Models/SoTempExcel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SoTempExcel extends Model
+{    
+    protected $table = 'so_temp_excel';
+    protected $guarded = [];
+}
--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,7 @@ Route::group(['namespace'=>'Api\Modules'],function(){
 
 Route::get('download-po-pdf/{id}','Api\Modules\Quote\QuoteController@downloadPdf')->name('downloadPdf');
 Route::post('sc_excel_submit','Api\Modules\Temp\TemporaryController@scExcelSubmit');
+Route::post('so-excel-submit','Api\Modules\Temp\SoTemporaryController@SoExcelSubmit');
 
 Route::group(['prefix' => 'user','middleware' => ['assign.guard:users', 'jwtmiddleware']],function ()
 {
