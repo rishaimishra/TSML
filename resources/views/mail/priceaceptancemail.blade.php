966b578dbb90feb2c197210be607755c39f894a9
Sourav 88 <srvmondal88@gmail.com>
2023-01-16 12:12:16 +0530

    price accept mail chngs

--- resources/views/mail/priceaceptancemail.blade.php
+++ resources/views/mail/priceaceptancemail.blade.php
@@ -33,7 +33,7 @@
         </tr>
         <tr>
             <td style="padding: 0 15px;">
-                <p style="font-weight: 400; font-size: 15px; line-height: 1.8; color: rgba(0,0,0,.8);">Congratulations! Our sales team has accepted the price, volume and delivery timeline against your RFQ. Please confirm the final quotation and upload your PO so that the order can be processed by our sales team.</p>
+                <p style="font-weight: 400; font-size: 15px; line-height: 1.8; color: rgba(0,0,0,.8);">Congratulations! Our sales team has accepted the price, volume and delivery timeline against your RFQ. Please upload your PO so that the order can be processed by our sales team.</p>
                
             </td>
         </tr>
